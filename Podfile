72e66cad20b888980e79f557723063c83c9b9d66
picopeak <liujiangning@gmail.com>
2015-11-30 11:07:14 +0800

    Add “Fuzi”, now we can support ios8.0, although Fuzi is not really being used. This is probably because Fuzi introduced new library, in which the original bug from Kanna has been fixed. They probably share the same xml lib.

--- Podfile
+++ Podfile
@@ -1,3 +1,5 @@
+platform :ios, '8.0'
 use_frameworks!
+pod 'Fuzi', '~> 0.3.0'
 pod 'Kanna', '~> 1.0.0'
 pod 'SQLite.swift', git: 'https://github.com/stephencelis/SQLite.swift.git'
--- Pods/Target Support Files/Pods/Pods-frameworks.sh
+++ Pods/Target Support Files/Pods/Pods-frameworks.sh
@@ -84,10 +84,12 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "Pods/Fuzi.framework"
   install_framework "Pods/Kanna.framework"
   install_framework "Pods/SQLite.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "Pods/Fuzi.framework"
   install_framework "Pods/Kanna.framework"
   install_framework "Pods/SQLite.framework"
 fi
